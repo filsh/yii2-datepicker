015285c530b93c3cff1a341560c654356625c8fb
Maliy Igor <imaliy.filsh@gmail.com>
2013-12-31 01:08:09 +0200

    Datepicker

--- DatePickerAsset.php
+++ DatePickerAsset.php
@@ -10,13 +10,13 @@ class DatePickerAsset extends \yii\web\AssetBundle
 {
     public $sourcePath = '@vendor/filsh/yii2-datepicker/yii/datepicker/assets';
 
+    public $js = [
+        'js/bootstrap-datepicker.js'
+    ];
+    
+    public $css = [
+        'css/datepicker.css'
+    ];
+    
     public $depends = ['yii\web\JqueryAsset'];
-
-    public function init()
-    {
-        $this->js[] = YII_DEBUG ? 'js/jquery.datepicker.js' : 'js/jquery.datepicker.min.js';
-        $this->css[] = YII_DEBUG ? 'css/datepicker.css' : 'js/datepicker.min.css';
-        
-        parent::init();
-    }
 }
